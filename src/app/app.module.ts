34b431e0fead72273ddca78442f00b4c2783aee8
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2023-03-09 06:00:29 +0000

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    TasksComponent,
+    TeamsComponent,
+    CalendarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
